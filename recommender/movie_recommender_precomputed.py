187f5b33d932cd11c11130597e30b0127c5b8818
arunnats <arun_b220019cs@nitc.ac.in>
2024-06-04 20:02:06 +0530

    restructured

--- recommender/movie_recommender_precomputed.py
+++ recommender/movie_recommender_precomputed.py
@@ -8,8 +8,8 @@ app = FastAPI()
 def load_precomputed_data():
     global app
     print("Loading precomputed correlation matrix and ratings...")
-    app.state.corr_matrix = pd.read_pickle('./correlation_matrix.pkl')
-    app.state.ratings = pd.read_pickle('./ratings.pkl')
+    app.state.corr_matrix = pd.read_pickle('./pickles/correlation_matrix.pkl')
+    app.state.ratings = pd.read_pickle('./pickles/ratings.pkl')
     print("Precomputed data loaded.")
 
 @asynccontextmanager
