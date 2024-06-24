d02be375ea587f2e019c020f97cf3a6ef572334a
arunnats <arun_b220019cs@nitc.ac.in>
2024-06-24 15:21:39 +0530

    Update top_books.sql

--- recommender/sql scripts/top_books.sql
+++ recommender/sql scripts/top_books.sql
@@ -10,10 +10,12 @@ CREATE TABLE top_books (
     avg_rating FLOAT
 );
 
+DROP TABLE top_books;
+
 SELECT COUNT(*) FROM top_books;
 CREATE INDEX titles ON top_books (`Book-Title`(255));
 ALTER TABLE top_books ADD FULLTEXT INDEX title_index (`Book-Title`);
 
-SELECT * FROM top_books WHERE `Book-Title` LIKE '%harry potter and the%' LIMIT 10;
+SELECT * FROM top_books WHERE `Book-Title` LIKE '%harry potter%';
 
 SELECT * FROM top_books WHERE `ISBN` = 0439064864;
