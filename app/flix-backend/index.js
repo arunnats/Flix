0c839ee83a5a32d5ee4c73886086f204ab01c750
arunnats <arun_b220019cs@nitc.ac.in>
2024-07-09 19:45:41 +0530

    added analytics

--- app/flix-backend/index.js
+++ app/flix-backend/index.js
@@ -31,18 +31,12 @@ app.get("/search", async (req, res) => {
 		return res.status(400).json({ error: "Query parameter 'q' is required" });
 	}
 
-	// Check cache first
 	if (cache.has(query)) {
 		console.log("Using cache for query:", query);
 		return res.json(cache.get(query));
 	}
 
 	try {
-		// const [results] = await pool.query(
-		// 	`SELECT Title, MovieId FROM movie WHERE MATCH(Title) AGAINST(? IN NATURAL LANGUAGE MODE) LIMIT 10`,
-		// 	[`${query}%`]
-		// );
-
 		const [results] = await pool.query(
 			`SELECT Title, MovieId FROM movie WHERE Title LIKE ? LIMIT 10`,
 			[`${query}%`]
@@ -60,7 +54,7 @@ app.get("/search", async (req, res) => {
 setInterval(() => {
 	cache.clear();
 	console.log("Cache cleared");
-}, 1000 * 60 * 60); // clear cache every hour
+}, 8000 * 60 * 60);
 
 const PORT = 5000;
 app.listen(PORT, () => {
